f3d983b6e58dedd4e461a590830e8ef92769bad7
manthri-kumar <153302220+manthri-kumar@users.noreply.github.com>
2026-01-25 23:50:36 +0530

    Fix Aiven MySQL SSL connection

--- index.js
+++ index.js
@@ -50,10 +50,10 @@ app.use(passport.session());
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false
   }
